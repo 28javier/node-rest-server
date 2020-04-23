e2c43dca2a49d19c75f5266b20edd7b8156f4f41
javierVL28 <javier19952009@gmail.com>
2020-04-23 14:52:54 -0500

    Primer commit

---
+++ server/config/config.js
@@ -0,0 +1,5 @@
+//================================================================
+//  Puerto configuracion para cuando trabaje en produccion o local
+//================================================================
+
+process.env.PORT = process.env.PORT || 3000;
---
+++ server/server.js
@@ -0,0 +1,53 @@
+//expres
+require('./config/config');
+
+
+const express = require('express');
+const app = express();
+//body-parce
+var bodyParser = require('body-parser');
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+
+
+//peticiones http
+
+app.get('/usuario', function(req, res) {
+    res.json('get Usuario');
+});
+
+app.post('/usuario', function(req, res) {
+    let body = req.body;
+    if (body.nombre === undefined) {
+        res.status(400).json({
+            ok: false,
+            mensaje: "El nombre es necesario"
+        });
+    } else {
+        res.status(200).json({
+            persona: body
+        });
+    }
+
+});
+
+app.patch('/usuario/:id', function(req, res) {
+    let id = req.params.id;
+    res.json({
+        id: id
+    });
+});
+
+app.delete('/usuario', function(req, res) {
+    res.json('delete Usuario');
+});
+
+
+//pueto del servidor
+app.listen(process.env.PORT, () => {
+    console.log('Escuchando en el puerto', process.env.PORT);
+
+});
