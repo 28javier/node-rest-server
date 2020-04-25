743c05d26608fb1811065a52c7a68956a4b5ef73
javierVL28 <javier19952009@gmail.com>
2020-04-24 19:21:01 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -17,7 +17,7 @@ let urlBD;
 if (process.env.NODE_ENV === 'dev') {
     urlBD = 'mongodb://localhost:27017/cafe';
 } else {
-    urlBD = 'mongodb+srv://javier:javiervl28.@cluster0-dxk5i.mongodb.net/cafe';
+    urlBD = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlBD;
